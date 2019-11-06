a099eab58b33224ab75c50df7ae3594dc84ad236
dimitris_tsiamasiotis <dtsiamasiotis@gmail.com>
2019-11-06 13:42:22 +0200

    initial commit for ticket actions

---
+++ servlets/servlet/src/main/java/ServletInit.java
@@ -0,0 +1,18 @@
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ServletInit extends Application {
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<Class<?>>();
+        classes.add(TicketActions.class);
+        return classes;
+    }
+
+    public Set<Object> getSingletons() {
+        // nothing to do, no singletons
+        return null;
+    }
+
+}
+
--- servlets/servlet/src/main/java/TicketActions.java
+++ servlets/servlet/src/main/java/TicketActions.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
+@Local
+@Path("/ticketActions")
 public class TicketActions {
+
+  @EJB
+  private LottoService lottoService;
+
+    @POST
+    @Path("createTicket")
+    public Response handleCreateTicket(Ticket newTicket)
+    {
+        String numbers = newTicket.getNumbers();
+        NumbersValidator numbersValidator = new NumbersValidator();
+        int validationResult = numbersValidator.checkNumbersString(numbers,6);
+        if(validationResult == 0) {
+            Participant participant = lottoService.findOrCreateParticipant(String.valueOf(newTicket.getParticipant().getMsisdn()));
+            boolean participantExists = true;
+            if (participant == null) {
+                participantExists = false;
+                participant = lottoService.createNewParticipant(newTicket.getParticipant().getMsisdn());
+            }
+
+            Ticket ticket = lottoService.createNewTicket(newTicket);
+            participant = lottoService.addTicketToParticipant(participant, ticket);
+
+
+            lottoService.saveOrUpdateParticipant(participant,participantExists);
+            return Response.ok("OK").build();
+        }
+        else{
+            return Response.ok(lottoService.createFailedResponseForNumbersValidation(validationResult)).build();
+        }
+    }
 }
